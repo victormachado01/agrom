b73a6fcb412f3728833ff8bb3518e27673f703a4
Davi Dutra <davidutramn@gmail.com>
2020-09-22 13:51:16 -0300

    feat(servico): criar serviço - hardcoded

--- api/src/controllers/servico.controller.js
+++ api/src/controllers/servico.controller.js
@@ -5,54 +5,76 @@ const ServicoService = require('../services/servico.service');
 
 module.exports = {
     cadastro: async (req, res, next) => {
-        const schema = Joi.object({
-            nome: Joi.string()
-                .min(4)
-                .max(200)
-                .trim()
-                .required(),
-            descricao: Joi.string()
-                .min(4)
-                .max(200)
-                .trim()
-                .required(),
-            valor: Joi.number()
-                .min(0)
-                .required(),
-            contato: Joi.string()
-                .min(8)
-                .max(12)
-                .trim()
-                .required(),
-            estado: Joi.string()
-                .trim()
-                .max(50)
-                .required(),
-            cidade: Joi.string()
-                .trim()
-                .max(200)
-                .required(),
-            IDCategoriaServico: Joi.number()
-                .integer()
-                .required()
-        });
+        try {
+            const schema = Joi.object({
+                nome: Joi.string()
+                    .min(4)
+                    .max(200)
+                    .trim()
+                    .required(),
+                descricao: Joi.string()
+                    .min(4)
+                    .max(200)
+                    .trim()
+                    .required(),
+                valor: Joi.number()
+                    .min(0)
+                    .required(),
+                contato: Joi.string()
+                    .min(8)
+                    .max(12)
+                    .trim()
+                    .required(),
+                estado: Joi.string()
+                    .trim()
+                    .max(50)
+                    .required(),
+                cidade: Joi.string()
+                    .trim()
+                    .max(200)
+                    .required(),
+                IDCategoriaServico: Joi.number()
+                    .integer()
+                    .required()
+            });
 
-        const { value, error } = schema.validate(req.body);
-        if (error) {
-            return res.status(400).send({ errors: error.details });
+            const { value, error } = schema.validate(req.body);
+            if (error) {
+                return res.status(400).send({ errors: error.details });
+            }
+
+            // Utilizar multer
+            const imgs = [
+                { path: '/home/davi/Pictures/agrom_v1.png' },
+                { path: '/home/davi/Pictures/agrom_v2.png' },
+            ];
+            
+            if (!imgs.length) {
+                return res.status(400).send({ error: "Obrigatório pelo menos uma imagem" });
+            } else if (imgs.length > 6) {
+                return res.status(400).send({ error: "Máximo de 6 imagens" });
+            }
+
+            value.IDUsuario = 1;
+
+            const servico = await ServicoService.cadastro(value, imgs);
+
+            return res.status(201).send({ servico });
+        } catch (error) {
+            console.error(error);
+            return res.status(500).send({ error: "Erro ao cadastrar serviço" });
         }
-
-        value.IDUsuario = 1;
-
-        const servico = await ServicoService.cadastro(value);
-
-        return res.status(201).send(value);
     },
     listar: async (req, res, next) => {
-        const page = parseInt(req.query.page) || 0;
+        try {
+            const page = parseInt(req.query.page) || 0;
 
-        const servicos = await ServicoService.listar(page);
+            const servicos = await ServicoService.listar(page);
 
-        return res.status(200).send({servicos: servicos.results, total: servicos.total});
+            return res.status(200).send({ servicos: servicos.results, total: servicos.total });
+        } catch (error) {
+            console.error(error);
+            return res.status(500).send({ error: "Erro ao listar serviços" });
+        }
     }
 }
--- api/src/services/servico.service.js
+++ api/src/services/servico.service.js
@@ -1,15 +1,34 @@
 const ServicoModel = require('../models/servico.model');
+const ImagemServicoModel = require('../models/imagemServico.model');
 
 class ServicoService {
-    async cadastro(value) {
-        const servico = await ServicoModel.query().insert(value);
+    async cadastro(servico, imagens) {
+        const result = await ServicoModel.transaction(async tx => {
+            const _servico = await ServicoModel
+                .query(tx)
+                .insert(servico);
 
-        return servico;
+            const _imagens = imagens.map(img => {
+                return ImagemServicoModel
+                    .query(tx)
+                    .insert({ path: img.path, IDServico: _servico.IDServico });
+            });
+
+            const imgs = await Promise.all([..._imagens]);
+
+            _servico.imagens = imgs;
+
+            return _servico;
+        })
+
+        return result;
     }
 
     async listar(page = 0, limit = 25) {
         // TODO: Adicionar filtros
-        const servicos = await ServicoModel.query().page(page, limit);
+        const servicos = await ServicoModel
+            .query()
+            .page(page, limit);
 
         return servicos;
     }
